3055a1231a8003ca1c2bfee1afa7c4950c1c12a1
AHorejsi <alex.horejsi59@gmail.com>
2025-02-21 13:52:29 -0800

    Added SSL config to project

--- Sudoku/src/main/kotlin/com/alexh/Application.kt
+++ Sudoku/src/main/kotlin/com/alexh/Application.kt
@@ -3,7 +3,10 @@ package com.alexh
 import io.ktor.server.netty.*
 import com.alexh.plugins.*
 import com.alexh.route.*
+import io.ktor.network.tls.certificates.*
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import java.io.File
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
