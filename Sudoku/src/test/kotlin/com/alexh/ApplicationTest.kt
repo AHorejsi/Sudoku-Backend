d111ae0385c098cd968fc92dc2b2cceab2b10263
AHorejsi <alex.horejsi59@gmail.com>
2025-02-22 18:57:43 -0800

    Resetting tests

--- Sudoku/src/test/kotlin/com/alexh/ApplicationTest.kt
+++ Sudoku/src/test/kotlin/com/alexh/ApplicationTest.kt
@@ -4,7 +4,6 @@ import io.ktor.client.request.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.http.*
-import com.alexh.route.configureRoutingForUsers
 import com.alexh.utils.Cookies
 import com.alexh.utils.Endpoints
 import com.alexh.utils.FormFields
@@ -18,80 +17,7 @@ class ApplicationTest {
 
     @Test
     fun testUsers() = testApplication {
-        this.application {
-            configureRoutingForUsers(this)
-        }
-
-        this@ApplicationTest.createUser(this)
-        this@ApplicationTest.getUserByUsername(this)
-        this@ApplicationTest.deleteUserByEmail(this)
-
-        this@ApplicationTest.createUser(this)
-        this@ApplicationTest.getUserByEmail(this)
-        this@ApplicationTest.deleteUserByUsername(this)
-    }
-
-    private suspend fun createUser(builder: ApplicationTestBuilder) {
-        val response = builder.client.put(Endpoints.CREATE_USER) {
-            this.header("Content-Type", "application/json")
-            this.setBody(FormDataContent(Parameters.build {
-                this.append(FormFields.USERNAME, "ahorejsi")
-                this.append(FormFields.PASSWORD, "abcd")
-                this.append(FormFields.EMAIL, "1111")
-            }))
-        }
-
-        assertEquals(HttpStatusCode.OK, response.status)
-    }
-
-    private suspend fun getUserByUsername(builder:ApplicationTestBuilder) {
-        val response = builder.client.get(Endpoints.GET_USER) {
-            this.header("Content-Type", "application/json")
-            this.setBody(FormDataContent(Parameters.build {
-                this.append(FormFields.USERNAME_OR_EMAIL, "ahorejsi")
-                this.append(FormFields.PASSWORD, "abcd")
-            }))
-        }
-
-        assertEquals(HttpStatusCode.OK, response.status)
-    }
-
-    private suspend fun deleteUserByEmail(builder:ApplicationTestBuilder) {
-        val response = builder.client.delete(Endpoints.DELETE_USER) {
-            this.header("Content-Type", "application/json")
-            this.cookie(Cookies.USER_ID, "1")
-            this.setBody(FormDataContent(Parameters.build {
-                this.append(FormFields.USERNAME_OR_EMAIL, "1111")
-                this.append(FormFields.PASSWORD, "abcd")
-            }))
-        }
-
-        assertEquals(HttpStatusCode.OK, response.status)
-    }
-
-    private suspend fun getUserByEmail(builder:ApplicationTestBuilder) {
-        val response = builder.client.get(Endpoints.GET_USER) {
-            this.header("Content-Type", "application/json")
-            this.setBody(FormDataContent(Parameters.build {
-                this.append(FormFields.USERNAME_OR_EMAIL, "1111")
-                this.append(FormFields.PASSWORD, "abcd")
-            }))
-        }
-
-        assertEquals(HttpStatusCode.OK, response.status)
-    }
-
-    private suspend fun deleteUserByUsername(builder:ApplicationTestBuilder) {
-        val response = builder.client.delete(Endpoints.DELETE_USER) {
-            this.header("Content-Type", "application/json")
-            this.cookie(Cookies.USER_ID, "1")
-            this.setBody(FormDataContent(Parameters.build {
-                this.append(FormFields.USERNAME_OR_EMAIL, "ahorejsi")
-                this.append(FormFields.PASSWORD, "abcd")
-            }))
-        }
-
-        assertEquals(HttpStatusCode.OK, response.status)
+        TODO()
     }
 
     @Test
