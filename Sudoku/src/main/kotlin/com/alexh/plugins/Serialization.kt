eadfc9dc2ca0fb5cf5c6f71b76e19baba1b53d5b
AHorejsi <alex.horejsi59@gmail.com>
2025-04-26 03:46:49 -0700

    Made JSON responses pretty print in dev mode

--- Sudoku/src/main/kotlin/com/alexh/plugins/Serialization.kt
+++ Sudoku/src/main/kotlin/com/alexh/plugins/Serialization.kt
@@ -3,8 +3,29 @@ package com.alexh.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun configureSerialization(app: Application) {
+    val config = app.environment.config
+
+    val devMode = config.property("ktor.development").getString().toBoolean()
+    val testMode = config.property("ktor.testing").getString().toBoolean()
+
+    if (devMode || testMode) {
+        configureDevSerialization(app)
+    }
+    else {
+        configureProdSerialization(app)
+    }
+}
+
+private fun configureDevSerialization(app: Application) {
+    app.install(ContentNegotiation) {
+        this.json(Json { this.prettyPrint = true })
+    }
+}
+
+private fun configureProdSerialization(app: Application) {
     app.install(ContentNegotiation) {
         this.json()
     }
